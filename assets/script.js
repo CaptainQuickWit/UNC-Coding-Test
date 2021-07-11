581d47e3d393a051ae4279ac5a8d500f5dfe337a
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-11 03:36:07 -0400

    added some div's and questions to be asked

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript",
+        choices: ["<HTML>", "<main>", "<javascript>", "<script>"],
+        answer: "<script>",
+        checked:false
+    },
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language", "HyperLinks and Text Markup Language", "Home Tool Markup Langauge", "I dont know"],
+        answer: "Hyper Text Markup Language",
+        checked:false
+    },
+    {
+        title: "What is an Array?",
+        choices: ["A single variable that lets you store multipel elements", "primitive data type", "a conditional statement", "all of the above"],
+        answer: "A single variable that lets you store multipel elements",
+        checked:false
+    },
+    {
+        title: "What is javascript",
+        choices: ["a prototype-based langauge", "a lanague having object oriented features", "both above", "none of the above"],
+        answer: "both above",
+        checked:false
+    },
+    {
+        title: "What is github?",
+        choices: ["the same as git", "the terminal / bash", "not important", "a collaboration site for programmers"],
+        answer: "a collaboration site for programmers",
+        checked:false
+    },
+  
+  ];
